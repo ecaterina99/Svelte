01c60ba297721e05f541604fb799257ba95cd085
ecaterina <ecaterinabjr@gmail.com>
2026-02-17 22:21:43 +0200

    Add style

---
+++ my-svelte-app/src/routes/Box.svelte
@@ -0,0 +1,14 @@
+<div class="box"></div>
+
+
+<style>
+    .box {
+        width: 100px;
+        height: 100px;
+        border-radius: 8px;
+        margin: 10px;
+
+        /* use CSS variable */
+        background-color: var(--color, gray);
+    }
+</style>
